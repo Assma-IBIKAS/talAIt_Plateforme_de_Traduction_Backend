aa75b4f54c608281235c1181a0f277bd8a7c4c49
Assma-IBIKAS <Assma-IBIKAS>
2025-11-25 21:05:26 +0100

    update schemas

---
+++ schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = 'bearer'
+
+
+class TranslateRequest(BaseModel):
+    text: str
+    direction: str # 'fr-en' or 'en-fr'
+
+
+class TranslateResponse(BaseModel):
+    translation: str
